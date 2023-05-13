907c2496627173fe0a81b7deb21b9319675deede
Michał Kalbarczyk <fazibear@gmail.com>
2023-05-13 15:32:33 +0200

    fixes

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ desc "Update Awesome Inesita"
 task :update_awesome do
   js_lib_url = 'https://raw.githubusercontent.com/inesita-rb/awesome-inesita/master/README.md'
   js_lib_dest = File.join(File.dirname(__FILE__), './source/includes/_awesome.md')
-  open(js_lib_url) do |f|
+  URI.open(js_lib_url) do |f|
     File.write(js_lib_dest, f.readlines.join)
   end
 end
--- config.rb
+++ config.rb
@@ -10,10 +10,12 @@ set :markdown,
     no_intra_emphasis: true
 
 # Assets
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+# set :css_dir, 'stylesheets'
+# set :js_dir, 'javascripts'
+# set :images_dir, 'images'
+# set :fonts_dir, 'fonts'
+
+set :sass_assets_paths, ['sources/stylesheets']
 
 activate :sprockets
 
@@ -34,7 +36,7 @@ set :relative_links, true
 configure :build do
   # If you're having trouble with Middleman hanging, commenting
   # out the following two lines has been known to help
-  activate :minify_css
+  # activate :minify_css
   activate :minify_javascript
   # activate :relative_assets
   # activate :asset_hash
